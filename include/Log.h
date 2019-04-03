c8c12bf6da6cc017bd9a26fc4c4176e647928d43
Callum <callum@ischus.co.uk>
2019-04-03 19:07:21 +0000

    Add get method to Log.cpp

--- include/Log.h
+++ include/Log.h
@@ -3,13 +3,31 @@
 #ifndef COBRA_NICKEL_LOG_H
 #define COBRA_NICKEL_LOG_H
 
+#include <ctime>
+#include <iostream>
+#include <sstream>
+
 namespace CobraNickel
 {
 	class Log
 	{
 	public:
 		Log();
-		~Log();
+		virtual ~Log();
+
+		static enum class Level {
+			Info,
+			Warning,
+			Error
+		};
+
+		const std::ostringstream& get(Level level);
+
+	protected:
+		std::ostringstream mOutputStream;
+
+	private:
+		Level mLevel;
 	};
 }
 
--- src/Log.cpp
+++ src/Log.cpp
@@ -1,1 +1,23 @@
 #include "Log.h"
+
+CobraNickel::Log::Log()
+{
+}
+
+CobraNickel::Log::~Log()
+{
+}
+
+const std::ostringstream& CobraNickel::Log::get(Level level)
+{
+	std::time_t t = std::time(0);
+	std::tm* now = std::localtime(&t);
+
+	mOutputStream << (now->tm_year + 1900) << '-';
+	mOutputStream << (now->tm_mon + 1) << '-';
+	mOutputStream << (now->tm_mday);
+
+	mLevel = level;
+
+	return mOutputStream;
+}
